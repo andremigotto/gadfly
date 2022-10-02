5c96ae18f5e72e84645116330aca77debd867c1e
Hmigotto <hmigotto@gmail.com>
2022-10-02 18:08:51 -0300

    criacao da tela de perguntas

--- pages/exam.js
+++ pages/exam.js
@@ -43,30 +43,79 @@ export default function ExamPage({ questoes, respostas }) {
 
   function validaResposta(resposta, id) {
     if(listaValidacao[resposta] == true) {
-      return true
+      const validacao = "sucesso"
+      return validacao
+    }
+  }
+  
+  //Funcao de escrever algo onde o Element ID é demo
+  var i = 0;
+  var txt = "aqui eu explico a pergunta, dizendo que por esse lado a pergunta foi explicada, e isso significa que a pergunta de fato foi sim explicada";
+  var speed = 50;
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
     }
   }
 
+  //Funcao para testar animacao do Soren Peter
+  const animacao = () => {
+    document.getElementById("proff").src='/Professor_Soren_Peter.gif'
+    setTimeout(() =>document.getElementById("proff").src='/Professor_Soren_Peter_parado.png', 1000)
+  }
+
   return (
-    <main>
-      <div className="box-center">
-        <h3>{listaPerguntas}</h3>
-            <button variant="outline-primary" active onClick={() => alternativa = validaResposta(0, "a")}>
-              {listaResposta[contadorAlternativas ++]}
-            </button>
-            <button variant="outline-primary" active onClick={() => alternativa = validaResposta(1, "b")}>
-              {listaResposta[contadorAlternativas ++]}
-            </button>
-            <button variant="outline-primary" active onClick={() => alternativa = validaResposta(2, "c")}>
-              {listaResposta[contadorAlternativas ++]}
-            </button>
-            <button variant="outline-primary" active onClick={() => alternativa = validaResposta(3, "d")}>
-              {listaResposta[contadorAlternativas ++]}
-            </button>
-            <button id="specialbutton" onClick={() => toast.success(alternativa)}>
-                DEBUG
-            </button>
-        </div>
-    </main>
+  <div className="menu-background">
+    
+  <div className="pergunta-box">
+    <div className="pergunta-box-img-area"><img src={'/Professor_Soren_Peter_parado.png'} id="proff" className="pergunta-box-img"
+      onClick={() => animacao()}/></div>
+    <div className="pergunta-box-text-area"><div className="pergunta-box-text" id="pergunta">pergnta aqui</div></div>
+  </div>
+
+    
+  <div className="Cartao">
+    <div className="Cartao-alternativas">
+      <h3 className="anuncio-alternativas">Alternativas</h3>
+      <div className="cartao-alternativas-selector" onClick={typeWriter}><p className="testo-alternativas">a)</p></div>
+      <div className="cartao-alternativas-selector" onClick={typeWriter}><p className="testo-alternativas">b)</p></div>
+      <div className="cartao-alternativas-selector" onClick={typeWriter}><p className="testo-alternativas">c)</p></div>
+      <div className="cartao-alternativas-selector" onClick={typeWriter}><p className="testo-alternativas">d)</p></div>
+    </div>
+    <div className="Cartao-divisoria"><button className="botao-confirmar-resposta">➡</button></div>
+    <div className="Cartao-xp">
+      <div className="explicacao_pergunta"><h5 className="testo-explicacao" id="demo"></h5></div>
+    </div>
+  </div>
+  </div>
+
+
+
+
+
+
+
+    // <main>
+    //   <div className="box-center">
+    //     <h3>{listaPerguntas}</h3>
+    //         <button variant="outline-primary" active onClick={() => alternativa = validaResposta(0, "a")}>
+    //           {listaResposta[contadorAlternativas ++]}
+    //         </button>
+    //         <button variant="outline-primary" active onClick={() => alternativa = validaResposta(1, "b")}>
+    //           {listaResposta[contadorAlternativas ++]}
+    //         </button>
+    //         <button variant="outline-primary" active onClick={() => alternativa = validaResposta(2, "c")}>
+    //           {listaResposta[contadorAlternativas ++]}
+    //         </button>
+    //         <button variant="outline-primary" active onClick={() => alternativa = validaResposta(3, "d")}>
+    //           {listaResposta[contadorAlternativas ++]}
+    //         </button>
+    //         <button id="specialbutton" onClick={() => toast.success(alternativa)}>
+    //             DEBUG
+    //         </button>
+    //     </div>
+    // </main>
   )
   }
